d6de87b7e0e8884316883b96d5a5895805a1924d
Flynn Buckingham <flynnham@users.noreply.github.com>
2017-04-22 04:11:28 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -12,10 +12,16 @@ then
 	cp i3config ~/.i3/config
 fi
 
-read -p "$sp Append Xresources? (Yy/*) - [WARNING: only do this once?]" -n 1 -r
+read -p "$sp Reconfigure Xresources? (Yy/*)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+	# backup incase stuff goes terribly wrong
+	cp ~/.Xresources ~/Xresources.old
+
+	# remove current configuration if present
+	sed '/! @START-FLYNNCONFIG@/,/! @END-FLYNNCONFIG@/d' ~/.Xresources > tmp; mv tmp ~/.Xresources
+	# append config to end of Xres
 	cat Xresources >> ~/.Xresources
 fi
 
